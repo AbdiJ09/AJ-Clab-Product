ab48209cf769f2aeece558c0dd84172fbbfdb073
Abdi <renah2017@gmail.com>
2023-05-13 23:05:11 +0700

    menambahkan contact dan footer

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,64 @@ sr.reveal(".about-text", { origin: "bottom" }, { delay: 400 });
 sr.reveal(".card", { delay: 400 });
 sr.reveal(".middle-text", { delay: 300 });
 sr.reveal(".product-shop", { origin: "bottom" }, { delay: 300 });
+sr.reveal(".image-container", { origin: "left" });
+sr.reveal(".text-contact", { origin: "top" });
+sr.reveal(".flex-input", { origin: "bottom" });
+sr.reveal(".message", { origin: "bottom" });
+sr.reveal(".btn-product", { origin: "bottom" });
+sr.reveal(".send", { origin: "left" });
+
+// send emaik
+function sendEmail(event) {
+  event.preventDefault(); // Mencegah aksi form default
+
+  // Mengambil nilai dari input form
+  var nama = document.getElementById("firstName").value;
+  var nama2 = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var pesan = document.getElementById("message").value;
+
+  // Validasi input form
+  if (
+    nama.trim() === "" ||
+    nama2.trim() === "" ||
+    email.trim() === "" ||
+    phone.trim() === "" ||
+    pesan.trim() === ""
+  ) {
+    alert("mohon lengkapi form dahulu");
+
+    return; // Menghentikan proses pengiriman email
+  }
+
+  // Mengirim email menggunakan EmailJS
+  emailjs
+    .send("service_yl5sxr9", "template_62tbfss", {
+      namaPenerima: "Abdi Janarianto",
+      namaPenerima: "Abdi J",
+      namaPengirim: nama,
+      namaPengirim: nama2,
+      email: email,
+      phone: phone,
+      pesan: pesan,
+    })
+    .then(
+      function (response) {
+        console.log("Email terkirim!", response.status, response.text);
+        // Redirect ke halaman sukses atau tampilkan pesan sukses
+        alert(`Terima kasih sudah mengirimkan pesan, ${nama}`);
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+      },
+      function (error) {
+        console.log("Kesalahan dalam mengirim email", error);
+        // Tampilkan pesan error atau lakukan tindakan lain jika terjadi kesalahan
+      }
+    );
+}
+
+// Fungsi untuk menampilkan popup
